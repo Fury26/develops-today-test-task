8b26418911c96365fc89f98483e9411f7553efd5
Fury26 <wizard10.vt@gmail.com>
2021-03-02 14:24:07 +0200

    renamed home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { AppLayout } from '../components/Layout';
 import { loadPosts } from '../redux/actions/postActions';
 import { NextPage } from 'next';
